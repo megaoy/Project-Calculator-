081303da6c41e052495bc60a9da2a7301d42740b
eduardo <edsocafe@yahoo.com.br>
2024-01-16 10:54:06 -0300

    basic function test

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("hello")
+function add(number1,number2)
+{
+    return number1 + number2;
+}
+
+function subtract(number1,number2)
+{
+    return number1 - number2;
+}
+
+function multiply(number1,number2)
+{
+    return number1 * number2;
+}
+
+function divide(number1,number2)
+{
+    return number1 / number2;
+}
+
+console.log("function add test: " + add(2,2));
+
+console.log("function subtract test: " + subtract(2,2));
+
+console.log("function multiply test: " + multiply(2,3));
+
+console.log("function divide test: " + divide(4,2));
